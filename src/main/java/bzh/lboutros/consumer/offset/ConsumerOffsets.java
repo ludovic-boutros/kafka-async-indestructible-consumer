42847e50f7a1c66d7457a4db620c64cac76ce669
Ludovic Boutros <lboutros@confluent.io>
2025-09-09 11:32:44 +0200

    Fix: Use concurrent map for offset management

--- src/main/java/bzh/lboutros/consumer/offset/ConsumerOffsets.java
+++ src/main/java/bzh/lboutros/consumer/offset/ConsumerOffsets.java
@@ -4,9 +4,9 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.OffsetAndMetadata;
 import org.apache.kafka.common.TopicPartition;
 
-import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
 
-public class ConsumerOffsets extends HashMap<TopicPartition, OffsetAndMetadata> {
+public class ConsumerOffsets extends ConcurrentHashMap<TopicPartition, OffsetAndMetadata> {
 
     public void incrementOffsetPosition(ConsumerRecord<?, ?> record) {
         put(new TopicPartition(record.topic(), record.partition()), new OffsetAndMetadata(record.offset() + 1));
